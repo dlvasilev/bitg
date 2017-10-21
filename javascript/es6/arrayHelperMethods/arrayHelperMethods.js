0288de67c94180d5b8b907de56f0d75b372c6b99
Daniel Vasilev <daniel.vasilev.dev@gmail.com>
2017-10-21 17:56:03 +0300

    Finishing up ES6 ArrayHelperMethods

--- javascript/es6/arrayHelperMethods/arrayHelperMethods.js
+++ javascript/es6/arrayHelperMethods/arrayHelperMethods.js
@@ -1,37 +1,41 @@
 /**
  * Array Helpers
  */
+console.log('ES6 Array Helpers');
+console.log('Fire them up with these commands: ');
 
 /**
  * forEach
  */
 // node 1_forEach.js
+console.log('$ node 1_forEach.js');
 
 /**
  * map
  */
-
+// node 2_map.js
+console.log('$ node 2_map.js');
 
 /**
  * filter
  */
-
+// node 3_filter.js
+console.log('$ node 3_filter.js');
 
 /**
  * find
  */
-
-
-/**
- * every
- */
-
+// node 4_find.js
+console.log('$ node 4_find.js');
 
 /**
- * some
+ * every and some
  */
-
+// node 5_every_and_some.js
+console.log('$ node 5_every_and_some.js');
 
 /**
  * reduce
  */
+// node 6_reduce.js
+console.log('$ node 6_reduce.js');
