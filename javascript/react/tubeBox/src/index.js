fbb7aa6efa75e00306f26da349a3fd8e2e78d1f9
Daniel Vasilev <daniel.vasilev.dev@gmail.com>
2017-10-25 20:43:42 +0300

    tubeBox add search functionality

--- javascript/react/tubeBox/src/index.js
+++ javascript/react/tubeBox/src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -16,19 +17,25 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
+    
+    this.videoSearch('rock');
+  }
 
-    YTSearch({key: YOUTUBE_API_KEY, term: 'rock'}, videos => {
+  videoSearch(term) {
+    YTSearch({term, key: YOUTUBE_API_KEY}, videos => {
       this.setState({
-        videos: videos,
+        videos,
         selectedVideo: videos[0]
       });
-    });   
+    });
   }
 
   render() {
+    const videoSearch = _.debounce(term => this.videoSearch(term), 500);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
