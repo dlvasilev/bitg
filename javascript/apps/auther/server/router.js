456ca40b492fd22c2871efbb8841cc574d657091
Daniel Vasiev <daniel.vasilev.dev@gmail.com>
2017-11-21 18:55:20 +0200

    Auther Api Server wrap up

--- javascript/apps/auther/server/router.js
+++ javascript/apps/auther/server/router.js
@@ -1,9 +1,19 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
   app.get('/', function(req, res, next) {
+    res.send(['Hi there!']);
+  });
+
+  app.get('/authed', requireAuth, function(req, res, next) {
     res.send(['001', { a: 'A', b: 'B'}, false]);
   });
 
+  app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
 }
