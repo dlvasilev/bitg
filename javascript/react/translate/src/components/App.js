0525cfbfd2f62a9d9e69366beaba0214df036760
Daniel Vasilev <daniel.vasilev.dev@gmail.com>
2019-09-06 14:57:58 +0300

    React Context with Context Store  Translation demo app v2

--- javascript/react/translate/src/components/App.js
+++ javascript/react/translate/src/components/App.js
@@ -1,36 +1,19 @@
 import React, { Component } from "react";
 import UserCreate from "./UserCreate";
-import LanguageContext from "../contexts/LanguageContext";
+import LanguageSelector from "./LangaugeSelector";
+import { LanguageStore } from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
 
 class App extends Component {
-  state = {
-    language: "english"
-  };
-
-  onLanguageChange = language => {
-    this.setState({ language });
-  };
-
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a language:
-          <i
-            className="flag us"
-            onClick={() => this.onLanguageChange("english")}
-          />
-          <i
-            className="flag nl"
-            onClick={() => this.onLanguageChange("dutch")}
-          />
-        </div>
-        <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
           <ColorContext.Provider value="green">
             <UserCreate />
           </ColorContext.Provider>
-        </LanguageContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
