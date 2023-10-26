1e81648f6da8d44d021666663add7c60bd5bf114
Daniel Vasilev <daniel.vasilev.dev@gmail.com>
2023-10-26 16:26:52 +0300

    .

--- javascript/react/pics/src/api.js
+++ javascript/react/pics/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const searchImages = async (query) => {
   const resp = await axios.get('https://api.unsplash.com/search/photos', {
     headers: {
-      Authorization: 'Client-ID e729e7665f936992a0626f347380eb07a3f33599e6eef60c8a4b1625883d0f37'
+      Authorization: 'Client-ID {SET_YOUR_KEY_HERE}'
     },
     params: {
       query
