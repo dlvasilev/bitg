0acccbad2123134d391652837312c682e4623f88
Daniel Vasilev <daniel.vasilev.dev@gmail.com>
2023-10-31 12:41:43 +0200

    books updates

--- javascript/react/books/src/App.js
+++ javascript/react/books/src/App.js
@@ -1,16 +1,33 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from './components/BookList';
 
 function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    setBooks([...books, { id: books.length + 1, title: title }]);
+    setBooks([...books, { id: Math.round(Math.random() * 9999), title }]);
+  }
+
+  const deleteBookById = (id) => {
+    setBooks(books.filter(book => {
+      return book.id !== id;
+    }));
+  }
+
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id == id) {
+        return { ...book, title: newTitle };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
   }
 
   return (
-    <div>
-      Number of Books: {books.length}
+    <div className="app">
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
